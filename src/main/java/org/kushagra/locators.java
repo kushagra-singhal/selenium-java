7ff8b09c782c2ccd9915e28691d882f000d21f48
kushagra-singhal <kushagra.singhalzemosolabs.com>
2023-03-02 10:31:21 +0530

    Initial commit

---
+++ src/main/java/org/kushagra/locators.java
@@ -0,0 +1,31 @@
+package org.kushagra;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class locators {
+    public static void main(String[] args) throws InterruptedException {
+       System.setProperty("webdriver.chrome.driver", "/home/kushas/Documents/drivers/chromedriver_linux64/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        //use implicit wait - 5 seconds time out
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // globally applicable adding timeout so that tests won't fail
+
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys("kushagra");
+        driver.findElement(By.name("inputPassword")).sendKeys("hello@123");
+        driver.findElement(By.className("signInBtn")).click();
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText()); //this fails due to time issues. browser takes some time to load but selenium doesn't
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000); // pausing the script for one second so that the transition has enough time to load
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
+        driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@s.in");
+        driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
+        driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("John@gmail.com");
+        driver.findElement(By.xpath("//form/input[3]")).sendKeys("1234567890");
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+     System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+    }
+}
---
+++ src/main/java/org/kushagra/seleniumIntroduction.java
@@ -0,0 +1,35 @@
+package org.kushagra;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class seleniumIntroduction {
+    public static void main(String[] args) {
+        //Invoking Browser
+        // chrome - chromeDriver --> methods
+        //ChromeDriver driver = new ChromeDriver(); this code might not work on other browsers
+        // webDriver methods
+        // chromedriver.exe -> ChromeBrowser
+        //webdriver.chrome.driver->  key to set property
+
+
+
+        //Selenium Manager replaces the setProperty from Selenium V4.6, now no use of setting key-value
+//        WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver", "/home/kushas/Documents/drivers/chromedriver_linux64/chromedriver");
+//        WebDriver driver = new ChromeDriver();  // here chrome driver is implementing WebDriver interface
+
+        //firefox launch
+        // gecko driver - webdriver.gecko.driver
+
+//        WebDriverManager.firefoxdriver().setup();
+        System.setProperty("webdriver.gecko.driver","/home/kushas/Documents/drivers/geckodriver-v0.32.2-linux64/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://amazon.in");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+//        driver.close();     // only the original window gets close
+        driver.quit();      // closes all associated windows opened by Selenium
+    }
+}
