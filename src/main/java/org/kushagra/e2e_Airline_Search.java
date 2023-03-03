efb38a637c4145aa6f5c664b6f294d100125266e
kushagra-singhal <kushagra.singhalzemosolabs.com>
2023-03-03 12:30:30 +0530

    e2e for airline flight search

---
+++ src/main/java/org/kushagra/e2e_Airline_Search.java
@@ -0,0 +1,47 @@
+package org.kushagra;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class e2e_Airline_Search {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/home/kushas/Documents/drivers/chromedriver_linux64/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();      // to run test in maximize
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_0")).click();
+        driver.findElement(By.xpath("//input[@id='ctl00_mainContent_ddl_originStation1_CTXT']")).click();
+        driver.findElement(By.xpath("//a[@value='BLR']")).click();
+        Thread.sleep(2000);     // origin
+        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();   //destination
+        driver.findElement(By.cssSelector(".ui-datepicker-days-cell-over.ui-datepicker-today")).click();    //today's date
+
+        if(driver.findElement(By.id("Div1")).getAttribute("style").contains("0.5"))     // one way trip
+        {
+            System.out.println("It's Disabled");
+            Assert.assertTrue(true);   // not selecting round trip : false
+        }
+        else
+        {
+            Assert.assertTrue(true);
+        }
+        driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();  //checkbox
+        driver.findElement(By.id("divpaxinfo")).click();
+        Thread.sleep(2000);
+        for(int i=1; i<5; i++)
+        {
+            driver.findElement(By.id("hrefIncAdt")).click();
+
+        }
+
+        driver.findElement(By.id("btnclosepaxoption")).click();     // done button
+        Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(),"5 Adult");
+        System.out.println(driver.findElement(By.id("divpaxinfo")).getText()); // after selecting
+        driver.findElement(By.id("ctl00_mainContent_btn_FindFlights")).click();
+
+
+
+    }
+}
