272f4774982554a21857c72782d5b18cd35ceb5e
kushagra-singhal <kushagra.singhalzemosolabs.com>
2023-03-08 08:34:10 +0530

    modifies added to cart problem

--- src/main/java/org/kushagra/AddingToCartProblem.java
+++ src/main/java/org/kushagra/AddingToCartProblem.java
@@ -1,52 +1,71 @@
 package org.kushagra;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
 public class AddingToCartProblem {
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "/home/kushas/Documents/drivers/chromedriver_linux64/chromedriver");
+        WebDriverManager.chromedriver().setup();
+        //System.setProperty("webdriver.chrome.driver", "/home/kushas/Documents/drivers/chromedriver_linux64/chromedriver");  // use selemium manager
         WebDriver driver = new ChromeDriver();
+        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);     // implicit wait
+        WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));     //explicit wait
+
 
         String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot", "Pumpkin"};    // string array
-        int j = 0;
 
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
         Thread.sleep(3000);
+        addItems(driver, itemsNeeded);
+        driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+        driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+        w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
+        driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.cssSelector("button.promoBtn")).click();
+        //explicit wait
+        w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+
+        System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
+
+    }
+
+    public static void addItems(WebDriver driver, String[] itemsNeeded) {
+        int j = 0;
         List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
-        for (int i =0; i<products.size(); i++)
-        {
+        for (int i = 0; i < products.size(); i++) {
             // Brocolli - 1 Kg  -> split - 1 kg
             // Brocolli,  1 kg
 
-           String[] veggies = products.get(i).getText().split("-");
-           String formattedVeggieName = veggies[0].trim();
+            String[] veggies = products.get(i).getText().split("-");
+            String formattedVeggieName = veggies[0].trim();
 
 
-           // format it to get actual vegetable name
+            // format it to get actual vegetable name
 
-           //check whether name you extracted is present in arrayList or not
+            //check whether name you extracted is present in arrayList or not
             // convert array into arrayList for easy search
             //
             List itemsNeededList = Arrays.asList(itemsNeeded);  // string array to arrayList
 
 
-           if(itemsNeededList.contains(formattedVeggieName))
-           {
-               j++;
-               driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
-               if(j==itemsNeeded.length){
-                   break;
-               }
+            if (itemsNeededList.contains(formattedVeggieName)) {
+                j++;
+                driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+                if (j == itemsNeeded.length) {
+                    break;
+                }
 
-           }
+            }
         }
-
     }
 
 }
