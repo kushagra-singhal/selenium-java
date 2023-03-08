feb43c9ecf1a3c0602b73a5ec9fa8b41051248f8
kushagra-singhal <kushagra.singhalzemosolabs.com>
2023-03-08 11:40:49 +0530

    Ajax Calls and childframes and Iframes

---
+++ src/main/java/org/kushagra/ActionsDemo.java
@@ -0,0 +1,27 @@
+package org.kushagra;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionsDemo {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.amazon.com/");
+        Actions a = new Actions(driver);       // to handle webpage interactions
+        WebElement move = driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
+
+        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform(); // to click on search and enter 'HELLO' in caps
+        //moves to specific element
+        a.moveToElement(move).contextClick().build().perform();  // need to build the step and add perform to execute, context click in for right click
+
+
+
+    }
+}
---
+++ src/main/java/org/kushagra/FramesTest.java
@@ -0,0 +1,26 @@
+package org.kushagra;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class FramesTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        //driver.manage().window().maximize();
+        driver.get("https://jqueryui.com/droppable/");
+        System.out.println(driver.findElements(By.tagName("iframe")).size()); // to get the number of frames present
+        driver.switchTo().frame(0); // switch to frames using index
+        //driver.switchTo().frame(driver.findElement(By.cssSelector("iframe.demo-frame")));  // now the control comes inside the frame
+        //driver.findElement(By.id("draggable")).click();  // this fails if we do not switch to frame in above step
+        Actions a = new Actions(driver);
+        WebElement source = driver.findElement(By.id("draggable"));
+        WebElement target = driver.findElement(By.id("droppable"));
+        a.dragAndDrop(source, target).build().perform();
+        driver.switchTo().defaultContent();   // imp step - to come out of frame content
+    }
+}
